ef699814d3aef41c2fd6788172a55545a8e9b09a
alex777artemov <56027008+alex777artemov@users.noreply.github.com>
2019-10-02 11:59:46 +0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -7,9 +7,4 @@ $('.account__arrow-up').on('click', function(){
 $('.nav__btn').on('click', function(){
   $(this).toggleClass('red');
   $('.left-menu').toggleClass('small-menu');
-  
-  
-  //$('.menu__link span, .theme__name, .copy').toggle();
-  //$('.menu__item').toggleClass('padding-right');
-  //$('.switch-btn').toggleClass('margin-zero');
 });
