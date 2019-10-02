55b005f652ee706a4fce7f2905e3250245795b64
alex777artemov <56027008+alex777artemov@users.noreply.github.com>
2019-10-03 05:59:30 +0600

    сделал меню и инпуты

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
-/*панель аккаунта*/
+/*Панель аккаунта*/
 $('.account__arrow-up').on('click', function(){
   $('.account-menu').toggle();
   $(this).toggleClass('rotate');
 });
-/*сворачивание левого меню*/
+/*Сворачивание левого меню*/
 $('.nav__btn').on('click', function(){
   $(this).toggleClass('red');
   $('.left-menu').toggleClass('small-menu');
 });
+/*красная иконка выбранного меню*/
+  $(".menu__link.current").find(".icon").addClass("icon-red");
+/*Подменю*/
+if($(".submenu__link").hasClass("current")){
+  $(".submenu__link.current").closest(".menu__item").find(".icon").addClass("icon-red");
+}
