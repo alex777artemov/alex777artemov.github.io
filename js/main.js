77d764f2e13101e6092c0392d569bd4b79339b12
alex777artemov <56027008+alex777artemov@users.noreply.github.com>
2019-10-04 13:12:26 +0600

    Добавил темную тему

--- js/main.js
+++ js/main.js
@@ -15,4 +15,15 @@ if($(".submenu__link").hasClass("current")){
   $(".submenu__link.current").closest(".menu__item").find(".icon").addClass("icon-red");
   $(".submenu__link.current").closest(".menu__item").find(".icon-arrow").addClass("icon-arrow_reverse");
 }
-
+/*Выбор темы*/
+$( ".theme__hidden-checkbox" ).on("change", function() {
+  if(!($(".theme__hidden-checkbox").prop("checked"))){
+    $("head").append("<link rel='stylesheet' href='css/black.css'>");
+    $(".header .logo__img").attr("src", "img/black-logo-header.svg");
+    $(".sign-in .logo__img").attr("src", "img/black-logo-header.svg");
+  } else {
+    $("link[href='css/black.css']").remove();
+    $(".header .logo__img").attr("src", "img/logo-header.svg");
+    $(".sign-in .logo__img").attr("src", "img/logo-header.svg");
+  }
+});
