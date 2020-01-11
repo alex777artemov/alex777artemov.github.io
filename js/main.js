7a85804c1d538863b16dcb04f6155a80c24e848d
alex777artemov <56027008+alex777artemov@users.noreply.github.com>
2020-01-12 02:13:13 +0600

    12

--- js/main.js
+++ js/main.js
@@ -1,58 +1,6 @@
-$(document).ready(function(){
-	//Мобильное меню
-	$('.open-mobile-menu').on('click', function(){
-		$('.popup-mob-menu').fadeIn();
-	});
-	$('.popup-mob-menu .popup__close').on('click', function(){
-		$('.popup-mob-menu').fadeOut();
-	});
-	//Сладеры на главной странице
-	$('.main-slider').slick({
-		infinite: false,
-		prevArrow: '<i class="arrow arrow-prev"></i>',
-		nextArrow: '<i class="arrow arrow-next"></i>',
-	});
-	$('.project-slider').slick({
-		infinite: false,
-		slidesToScroll: 1,
-		slidesToShow: 1,
-		variableWidth: true,
-		swipeToSlide: true
-	});
-	$('.ensemble-slider').slick({
-		centerMode: true,
-		slidesToShow: 3,
-		swipeToSlide: true,
-		focusOnSelect: true,
-		//centerPadding: 50,
-		centerPadding: 10,
-		//rtl: true,
-		prevArrow: '<i class="arrow arrow-prev"></i>',
-		nextArrow: '<i class="arrow arrow-next"></i>',
-		responsive: [
-			{
-				breakpoint: 1240,
-				settings: {
-					centerMode: false,
-					slidesToShow: 2,
-				}
-			},
-			{
-				breakpoint: 768,
-				settings: {
-					centerMode: false,
-					slidesToShow: 1,
-				}
-			},
-		]
-	});
-	$('.project-page-gallery-slider').slick({
-		prevArrow: '<i class="arrow arrow-prev"></i>',
-		nextArrow: '<i class="arrow arrow-next"></i>',
-		variableWidth: true,
-		centerMode: true,
-		//slidesToShow: 1,
-		swipeToSlide: true,
-		focusOnSelect: true,
-	});
-});
+$('.owl-carousel').owlCarousel({
+    margin:10,
+    loop:true,
+    autoWidth:true,
+    items:4
+})
